1a6623be947fa203577d3796499b2037f4b0687c
Jacob Tomaw <jacob.tomaw@orbitz.com>
2015-01-24 16:28:13 -0600

    Initial Commit

---
+++ Scavenger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scavenger
+//
+//  Created by Jacob Tomaw on 1/24/15.
+//  Copyright (c) 2015 Jacob Tomaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
